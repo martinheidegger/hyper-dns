1b852594ae6037726bd62304f36afb83ecc1e92f
Martin Heidegger <martin.heidegger@gmail.com>
2021-04-20 16:21:34 +0900

    adding hyper protocol

--- protocols.js
+++ protocols.js
@@ -10,6 +10,21 @@ module.exports = Object.freeze({
     }
     return await context.fetchWellKnown(name, 'dat', /^\s*(?:(?:dat):)?(?:\/\/)?(?<key>[0-9a-f]{64})\s*$/i, 6)
   },
+  async hyper (context, name) {
+    let record = context.matchRegex(name, /^(?<key>[0-9a-f]{64})$/i)
+    if (record !== undefined) {
+      return record
+    }
+    record = await context.getDNSTxtRecord(name, /^\s*"?hyperkey=(?<key>[0-9a-f]{64})"?\s*$/i)
+    if (record !== undefined) {
+      return record
+    }
+    record = await context.getDNSTxtRecord(name, /^\s*"?datkey=(?<key>[0-9a-f]{64})"?\s*$/i)
+    if (record !== undefined) {
+      return record
+    }
+    return await context.fetchWellKnown(name, 'dat', /^\s*(?:(?:dat):)?(?:\/\/)?(?<key>[0-9a-f]{64})\s*$/i, 6)
+  },
   async cabal (context, name) {
     let record = context.matchRegex(name, /^(?<key>[0-9a-f]{64})$/i)
     if (record !== undefined) {
