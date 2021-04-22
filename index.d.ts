80c787e235286ade4cc9dcfb7956ec221495f8a6
Martin Heidegger <martin.heidegger@gmail.com>
2021-04-22 14:54:46 +0900

    fix: making sure that the typescript resolve picks the right properties

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import resolve, { Cache, ProtocolInput, ResolveOptions, ResolveURLOptions, LightURL } from './resolve'
 import createCacheSQLite from './cache-sqlite'
 
-declare const hyperdns: typeof resolve & {
+declare const hyperdns: Omit<typeof resolve, 'resolve' | 'resolveProtocol' | 'resolveURL'> & {
   cache: Cache
   createCacheSQLite: typeof createCacheSQLite
   resolve (input: string, opts: ResolveOptions): Promise<string | null>
